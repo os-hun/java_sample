a685e8b97f19a0732da53fe57611c65bf0ae836d
ShunOmine <shun12baseball@gmail.com>
2020-04-17 10:53:23 +0900

    first commit

---
+++ MethodTest.java
@@ -0,0 +1,22 @@
+public class MethodTest {
+    public static void main(String[] args){
+        String 魔女の名前 = "Latifa";
+        byte hp = 100;
+        short mp = 50;
+        int offense = 9999;
+        long defense = -100000;
+        float speed = 1.5f;
+        double lac = -0.832712;
+        char code = 'L';
+        boolean isWoman = true;
+        System.out.println(魔女の名前);
+        System.out.println(hp);
+        System.out.println(mp);
+        System.out.println(offense);
+        System.out.println(defense);
+        System.out.println(speed);
+        System.out.println(lac);
+        System.out.println(code);
+        System.out.println(isWoman);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+//コンソールに表示するためのコード。
+public class Test{
+    public static void main(String[] arg){
+        System.out.println("私はのんぼるです。");
+        System.out.println("テトリスを作って見ます。");
+        System.out.println("でもやっぱりシューティングゲームを作ります。");
+    }
+}
+
